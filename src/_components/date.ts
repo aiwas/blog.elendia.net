f1ecdc22c496872eda84c13db4c7f8ef0bb77e47
Miyako Kuwano <aiwas@arg.vc>
2024-01-13 05:12:52 +0900

    日付がずれるのでPteraでTimezone対応

--- src/_components/date.ts
+++ src/_components/date.ts
@@ -1,9 +1,10 @@
-import { format } from "std/datetime/format.ts";
+import { datetime } from "https://deno.land/x/ptera@v1.0.2/mod.ts";
 
 type Props = {
   date: Date;
 };
 
 export default ({ date }: Props) => {
-  return /*html*/ `<time dateTime=${date.toISOString()}>${format(date, "yyyy/MM/dd")}</time>`;
+  const d = datetime(date).toZonedTime("Asia/Tokyo");
+  return /*html*/ `<time dateTime=${d.toISO()}>${d.format("YYYY/MM/dd")}</time>`;
 };
--- src/_components/jsx/DateTag.tsx
+++ src/_components/jsx/DateTag.tsx
@@ -1,9 +1,10 @@
-import { format } from "std/datetime/format.ts";
+import { datetime } from "https://deno.land/x/ptera@v1.0.2/mod.ts";
 
 type Props = {
   date: Date;
 };
 
-export default function DateTag(props: Props) {
-  return <time dateTime={props.date.toISOString()}>{format(props.date, "yyyy/MM/dd")}</time>;
+export default function DateTag({ date }: Props) {
+  const d = datetime(date).toZonedTime("Asia/Tokyo");
+  return <time dateTime={d.toISO()}>{d.format("YYYY/MM/dd")}</time>;
 }
