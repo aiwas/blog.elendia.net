2e26634cdbd3ce04d2876762d4b27ada26f03c52
Miyako Kuwano <aiwas@arg.vc>
2024-07-07 02:20:16 +0900

    2ページ目がなくてもMoreリンクが出ていたのを修正

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,14 @@ import PostList from "./_components/jsx/PostList.tsx";
 export const layout = "layouts/base.vto";
 
 export default ({ search }: Lume.Data) => {
-  const postList = search.pages("type=post", "date=desc", 10);
+  const postList = search.pages("type=post", "date=desc", 11);
   return (
     <main>
-      <PostList dataList={postList} />
+      <PostList dataList={(postList.length > 10) ? postList.slice(0, -1) : postList} />
       <hr />
       <nav className="nav">
         <div className="nav-left">
-          <a href="/posts/page/2">More</a>
+          {(postList.length > 10) ? <a href="/posts/page/2">More</a> : ""}
           <a href="/tags/">Tags</a>
         </div>
       </nav>
